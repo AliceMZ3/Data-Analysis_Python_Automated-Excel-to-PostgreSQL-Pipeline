6f11da8aa18cdcb62fd95208af39eeb4a8cefac6
Jose Marin <josemarin191@gmail.com>
2026-08-07 21:27:41 -0600

    Expanded data analysis
    Printing core dataset attributes on run

--- main.py
+++ main.py
@@ -6,12 +6,28 @@ FILE_PATH = 'data/raw/sales.xlsx'
 def main():
     df = pd.read_excel(FILE_PATH)
 
-    print(df)
-
     print('\n Dataset info:')
     df.info()
 
     print('\nStat summary:')
     print(df.describe())
 
+    print("\nFirst five rows:")
+    print(df.head())
+
+    print('\nDataset dimentions/shape')
+    print(f"Rows: {df.shape[0]}")
+    print(f'Columns: {df.shape[1]}')
+
+    print('\nColumn names:')
+    print(df.columns.to_list())
+
+    print('\nData types:')
+    print(df.dtypes)
+
+    print('\nMissing values:')
+    print(df.isnull().sum())
+
+    print('\nDuplicate rows:')
+    print(df.duplicated().sum())
 main()
