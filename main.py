f0e8c80d0a8139469f26ed13642bce2928ec9616
Jose Marin <josemarin191@gmail.com>
2026-08-07 21:18:36 -0600

    Initial reading of a non-messy excel file

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 
-if __name__=='__main__':
-    print('starting')
+import pandas as pd
+
+FILE_PATH = 'data/raw/sales.xlsx'
+
+def main():
+    df = pd.read_excel(FILE_PATH)
+
+    print(df)
+
+    print('\n Dataset info:')
+    df.info()
+
+    print('\nStat summary:')
+    print(df.describe())
+
+main()
